483bc47e3919898095c9c0306ef6dfd6cbe6c370
Arsalan Arghavan <arsalanarghavan2014@gmail.com>
2025-11-16 23:16:06 +0330

    Add assets, fix setup wizard and CORS

--- backend/api-gateway/bootstrap/app.php
+++ backend/api-gateway/bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
